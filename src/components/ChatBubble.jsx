ad7c7bf8c356a189a3abb09990526014ddd48b56
Aditya Pimpale <pimpaleaditya2@gmail.com>
2025-09-28 23:25:58 +0530

    # udpate for bubble

--- src/components/ChatBubble.jsx
+++ src/components/ChatBubble.jsx
@@ -1,25 +1,22 @@
 // components/ChatBubble.jsx
 import React from "react";
-import "../styles.css"; // Create or update this CSS file
+import "../styles.css";
 
 function ChatBubble({ text, images, type }) {
-  // Define avatar image paths or URLs
-  const userAvatar = "../../public/assets/user_bot/Profile-Avatar-PNG.png"; // Replace with your user avatar image path or URL
-  const botAvatar = "../../public/assets/user_bot/12239772.png";   // Replace with your bot avatar image path or URL
+  const userAvatar = "/assets/user_bot/Profile-Avatar-PNG.png"; 
+  const botAvatar = "/assets/user_bot/12239772.png";
 
   return (
     <div className={`chat-bubble-wrapper ${type}`}>
       <div className={`chat-bubble-container ${type}`}>
-        {/* Avatar */}
         <img
           src={type === "user" ? userAvatar : botAvatar}
           alt={type === "user" ? "User" : "Bot"}
           className="avatar"
         />
-        {/* Message Content */}
         <div className="chat-bubble">
           <div className="text">{text}</div>
-          {images && images.length > 0 && (
+          {images?.length > 0 && (
             <div className="images">
               {images.map((img, index) => (
                 <img
